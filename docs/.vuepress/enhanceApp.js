76f3ca16c1bbaddfaa240db27c7a98085f9045d0
Carl Summers <summers.carl@gmail.com>
2020-06-25 22:48:12 -0400

    Fix missing component

--- docs/.vuepress/enhanceApp.js
+++ docs/.vuepress/enhanceApp.js
@@ -1,10 +1,9 @@
-const Admin = () => import("./Admin");
-
-export default ({ Vue, options, router }) => {
-  router.addRoutes([
-    {
-      path: "/admin",
-      component: Admin
-    }
-  ]);
-};
+export default ({
+  Vue, // the version of Vue being used in the VuePress app
+  options, // the options for the root Vue instance
+  router, // the router instance for the app
+  siteData, // site metadata
+  isServer // is this enhancement applied in server-rendering or client
+}) => {
+  // ...apply enhancements to the app
+}
